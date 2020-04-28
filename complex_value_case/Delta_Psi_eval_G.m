f4a4908eb8edcd859523b3dd566dd2a653072ec2
StefanoFor <61187718+StefanoFor@users.noreply.github.com>
2020-04-28 17:03:32 +0200

    Update Delta_Psi_eval_G.m

--- complex_value_case/Delta_Psi_eval_G.m
+++ complex_value_case/Delta_Psi_eval_G.m
@@ -4,15 +4,15 @@ function [Delta_T, Psi_T] = Delta_Psi_eval_G(y, T)
 
 [kernel_vect,u] = kernel_rank_sign_G(y,T);
 
-T2 = kron(T.',T);
+inv_sr_T = inv(sqrtm(T));
 
-sr_T2 = sqrtm(T2);
+inv_sr_T2 = kron(inv_sr_T.',inv_sr_T);
 
 I_N = eye(N);
 
 J_n_per = eye(N^2) - I_N(:)*I_N(:).'/N;
 
-K_V = (sr_T2\J_n_per);
+K_V = (inv_sr_T2*J_n_per);
 
 % Kernel_appo = zeros(N^2,1);
 % for k=1:K
@@ -24,7 +24,7 @@ Mat_appo = u .* reshape( u', [1 K N] );
 Mat_appo = reshape( permute( Mat_appo, [1 3 2] ), N^2, [] );
 Kernel_appo = Mat_appo*kernel_vect.';
 
-Delta_T = -(sr_T2\J_n_per)*Kernel_appo/sqrt(K);
+Delta_T = -K_V*Kernel_appo/sqrt(K);
 Delta_T = Delta_T(2:end);
 
 K_c = K_V*K_V';
