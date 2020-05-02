d04aae3d23cb0d13d3da6c582fae0fc2116d72ba
StefanoFor <61187718+StefanoFor@users.noreply.github.com>
2020-05-02 21:53:50 +0200

    Update jacobian_constraint.m

--- real_value_case/jacobian_constraint.m
+++ real_value_case/jacobian_constraint.m
@@ -1,20 +1,9 @@
 function J_phi = jacobian_constraint(N)
 
-%% Real
 J_phi = zeros(N*(N+1)/2,1);
 
 for j = 1:N-1;
     index_con(j) = 1 + N*(j-1) - (j-1)*(j-2)/2;
 end
-% 
-% J_phi(N*(N+1)/2,index_con) = -1;
-% J_phi(N*(N+1)/2,N*(N+1)/2) = 0;
-
-%% Complex
-% J_phi = zeros(N^2,1);
-% 
-% for j = 1:N;
-%     index_con(j) = j + N*(j-1);
-% end
 
 J_phi(index_con,1) = 1;
