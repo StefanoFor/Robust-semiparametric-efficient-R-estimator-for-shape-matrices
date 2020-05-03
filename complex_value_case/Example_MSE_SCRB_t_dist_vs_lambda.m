d94a8c400690b5a80d5b322254234747c189a3fa
StefanoFor <61187718+StefanoFor@users.noreply.github.com>
2020-05-03 16:39:36 +0200

    Update Example_MSE_SCRB_t_dist_vs_lambda.m

--- complex_value_case/Example_MSE_SCRB_t_dist_vs_lambda.m
+++ complex_value_case/Example_MSE_SCRB_t_dist_vs_lambda.m
@@ -144,7 +144,7 @@ hold on
 semilogy(lambdavect,Fro_MSE_NFP,line_marker{4},'LineWidth',1,'Color',color_matrix(4,:),'MarkerEdgeColor',color_matrix(4,:),'MarkerFaceColor',color_matrix(4,:),'MarkerSize',8);
 grid on;
 semilogy(lambdavect,Fro_MSE_RM,line_marker{5},'LineWidth',1,'Color',color_matrix(5,:),'MarkerEdgeColor',color_matrix(5,:),'MarkerFaceColor',color_matrix(5,:),'MarkerSize',8);
-axis([1 20 0.3 0.6])
+axis([1 20.1 0.3 0.6])
 xlabel('Shape parameter: \lambda');ylabel('Frobenius norm');
 legend('CCRB','CSCRB','CSCM','C-Tyler','R-est')
 title('MSE in Frobenus norm')
